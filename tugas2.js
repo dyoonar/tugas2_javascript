828218946dfd7819e9d80b1b6e1e35cd4222d062
dyoonar <71246827+dyoonar@users.noreply.github.com>
2020-09-18 03:36:50 +0000

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var nip = 12345
+var nama = "dika"
+var nikah = true
+var q = null
+var oray = ["1", "2", "3"]
+
+console.log (nip)
+console.log (nama)
+console.log (nikah)
+console.log (q)
+console.log (oray)
